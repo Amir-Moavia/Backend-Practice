90bbc0449dff2f4dbb972eb446fa4a6dc77d6849
Amir-Moavia <amirmoavia969@gmail.com>
2026-09-01 23:20:23 +0500

    some updates in the files

--- First-folder/index.js
+++ First-folder/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
-if (require.main === module) {
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
-}
+app.get('/button', (req, res) => {
+  res.send('<button>Click Me!</button>')
+})
 
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
