6f25f400a5e8fd80b91180dff560dd7db9f64d4c
Amir-Moavia <amirmoavia969@gmail.com>
2026-09-02 21:35:21 +0500

    Updating the backend files

---
+++ Second_Folder/front-end/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import axios from 'axios';
+import { useEffect } from 'react'
+
+
+function App() {
+  const [Jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/jokes')
+    .then((response) => {
+      setJokes(response.data)
+    }).catch((error) =>{
+      console.log(error)
+    })
+  })
+
+  return (
+    <>
+     <h1>Hello there</h1>
+     <p>Jokes : {Jokes.length}</p>
+
+     {
+        Jokes.map((Jokes,index) => {
+          <div key={Jokes.id}>
+            <h3>{}</h3>
+          </div>
+        })
+     }
+    </>
+  )
+}
+
+export default App
