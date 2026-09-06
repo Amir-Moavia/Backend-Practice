216f2f9f8975106637508e34f3c34aee26b08c1a
Amir-Moavia <amirmoavia969@gmail.com>
2026-09-06 23:58:51 +0500

    refactor: invoke connectDB function in index.js for immediate database connection

--- Fourth-folder/src/index.js
+++ Fourth-folder/src/index.js
@@ -3,15 +3,25 @@
 
 import dotenv from 'dotenv';
 import connectDB  from './db/index.js';
+//import express from 'express';
+//const app = express();
 
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 
 connectDB();
-
+// .then(()=>{
+//     app.listen(process.env.PORT || 8000, () => {
+//         console.log(`Server is running at port : ${process.env.PORT}`);
+        
+//     })
+// }).catch((error) => {
+//     console.log(`Mongo db connection failded !!! `, error);
+    
+// });
 
 
 
