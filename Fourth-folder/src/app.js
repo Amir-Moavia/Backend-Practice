47ad90f5c3bd21062e04fe9ab5b0b167d2e320d7
Amir-Moavia <amirmoavia969@gmail.com>
2026-09-07 19:51:24 +0500

    feat: add ApiResponse class for standardized API responses; update comments in app.js for clarity

--- Fourth-folder/src/app.js
+++ Fourth-folder/src/app.js
@@ -11,11 +11,11 @@ app.use(cors({
     credentials: true,
 }))
 
-app.use(express.json({limit: '16kb'}));
+app.use(express.json({limit: '16kb'})); // this is for defensive measures like to stop someone from being sending the 5 gb request data so the server may crash or consume server resources.
 
-app.use(express.urlencoded({extended: true, limit: '16kb'}));
+app.use(express.urlencoded({extended: true, limit: '16kb'})); // controls how complex URL-encoded data is parsed.
 
-app.use(express.static('/public'));
+app.use(express.static('public'));
 
 app.use(cookieParser()); 
 
