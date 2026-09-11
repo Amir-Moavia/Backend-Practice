d9e2ad31080ac06bb589d5b7249bad59580a4bde
Amir-Moavia <amirmoavia969@gmail.com>
2026-09-11 22:16:01 +0500

    feat: add file upload middleware to user registration route

--- Fourth-folder/src/controllers/user.controllers.js
+++ Fourth-folder/src/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 import { asyncHandler  } from "../utils/asyncHandler.js";
 import { User } from '../models/user.model.js';
+import { ApiError } from '../utils/apiError.js';
 
 const registerUser = asyncHandler( async (req, res) => {
     
@@ -12,12 +13,46 @@ const registerUser = asyncHandler( async (req, res) => {
     console.log("Email : ", email);
     // here if the clint sent the request then the data will come to the server but
     // over there the clint will be waiting for the response so we will add
-    
+
     res.status(200).json({
         success: true,
         message: "Data received successfully!"
     });
+
+    // if (fullName === "") {
+    //     throw new ApiError(400, "Full Name Is Required!!!" );   
+    // }
+    // if (email === "") {
+    //     throw new ApiError(400, "Email Is Required!!!" );   
+    // }
+    // if (username === "") {
+    //     throw new ApiError(400, "Username Is Required!!!" );   
+    // }
+    // if (password === "") {
+    //     throw new ApiError(400, "Password Is Required!!!" );   
+    // }
+
+    // we can also write the above code as 
+    if ([fullName,email,username,password].some((field) => 
+    {
+       return field?.trim() === "";
+    })) {
+        throw new ApiError(400, "All fields are required!!");
+    }
+
+    const existedUser = User.findOne(
+        {
+            $or: [{ username }, { email }]
+        }
+    )
+
+    if(existedUser)
+    {
+        throw new ApiError(409, "The username or email already existed");
+    }
+
+
     
 });
 
-export { registerUser };
+export { registerUser };  
