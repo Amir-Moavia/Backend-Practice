b30074e24080652e497497aa4fd3828fbab6901e
Amir-Moavia <amirmoavia969@gmail.com>
2026-09-11 19:39:01 +0500

    feat: enhance registerUser controller to process user data and improve response message

--- Fourth-folder/src/controllers/user.controllers.js
+++ Fourth-folder/src/controllers/user.controllers.js
@@ -1,9 +1,23 @@
 import { asyncHandler  } from "../utils/asyncHandler.js";
+import { User } from '../models/user.model.js';
 
 const registerUser = asyncHandler( async (req, res) => {
+    
+    const { fullName, email, username, password } = req.body;
+    //this is equal to 
+    // const fullName = req.body.fullName;
+    // const email = req.body.email;
+    // const username = req.body.username;
+    // const password = req.body.password;
+    console.log("Email : ", email);
+    // here if the clint sent the request then the data will come to the server but
+    // over there the clint will be waiting for the response so we will add
+    
     res.status(200).json({
-        message: "Ok",
-    })
+        success: true,
+        message: "Data received successfully!"
+    });
+    
 });
 
 export { registerUser };
