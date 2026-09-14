6b087519e4127066bc7fb36315c8d7deeb55ebe4
Amir-Moavia <amirmoavia969@gmail.com>
2026-09-14 10:50:56 +0500

    feat: implement token generation and cookie handling in loginUser function

--- Fourth-folder/src/controllers/user.controllers.js
+++ Fourth-folder/src/controllers/user.controllers.js
@@ -4,6 +4,28 @@ import { ApiError } from '../utils/apiError.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 
+
+const generateAccessAndRefreshToken = async(userId) =>
+{
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false})
+
+        return {accessToken, refreshToken};
+        
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating refresh and access tokens");
+
+    }
+}
+
+
+
+
 const registerUser = asyncHandler( async (req, res) => {
 
 
@@ -146,7 +168,27 @@ const loginUser = asyncHandler( async (req, res) => {
         throw new ApiError(401, "The Password is incorrect");
     }
 
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
 
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    );
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        200,
+        {
+            user: loggedInUser, accessToken,
+            refreshToken
+        },"User logged In Successfully"
+    )
 
 })
 
